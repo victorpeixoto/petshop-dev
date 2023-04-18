bb53a75e20456171e15641232db14e3e4e9b9611
Victor Peixoto <victornovaisp@gmail.com>
2023-04-18 17:22:11 -0400

    feat: configuração de rotas, controllers

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,9 @@
-import express from 'express';
+import express, {Request, Response} from 'express';
 import dotenv from 'dotenv';
 import mustache from 'mustache-express';
 import path from 'path';
+
+import mainRoutes from './routes';
 
 dotenv.config();
 const server = express();
@@ -13,6 +15,13 @@ server.use(express.static(path.join(__dirname, '../public')));
 
 //! Rotas
 
+server.use(mainRoutes);
+
+
+
+server.use((req: Request, res: Response) => {
+  res.status(404).send('Página não encontrada!')
+})
 
 
 server.listen(process.env.PORT);
